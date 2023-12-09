6e13819d4d169b8ff7b6d9165099c546a8bed507
Donatas Petrauskas <donatas.petr@gmail.com>
2023-12-09 21:37:45 +0200

    recover day6

--- 2023/day6.el
+++ 2023/day6.el
@@ -4,17 +4,27 @@
 
 
 ;; day 6 part 1
-(with-current-buffer (find-file-noselect "~/tmp/day6.example.txt")
+(with-current-buffer (find-file-noselect "~/tmp/day6.txt")
   (labels ((number (str) (string-to-number str))
            (numbers (str) (mapcar #'number (string-split str " +"))))
     (pcase-exhaustive (string-split (buffer-string) "[\n:]" t " +")
       (`(,_ ,times ,_ ,records)
-       (-zip-with (lambda (time record)
-                    (list time record))
-                  (numbers times)
-                  (numbers records))))))
+       (apply #'* (-zip-with (lambda (time record)
+                               (loop for i from 1 below time
+                                     count (< record (* i (- time i)))))
+                             (numbers times)
+                             (numbers records))))))) ;; 800280
 
 
 
 ;; day 6 part 2
 
+(with-current-buffer (find-file-noselect "~/tmp/day6.txt")
+  (labels ((number (str) (string-to-number str))
+           (numbers (str) (mapcar #'number (string-split str " +"))))
+    (pcase-exhaustive (string-split (buffer-string) "[\n:]" t " +")
+      (`(,_ ,times ,_ ,records)
+       (let ((time (number (string-replace " " "" times)))
+             (record (number (string-replace " " "" records))))
+         (loop for i from 1 below time
+               count (< record (* i (- time i))))))))) ;; 45128024
