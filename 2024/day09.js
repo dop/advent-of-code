b397ba8d93e876ff760b1a267a7a70fab4bbb37d
Donatas Petrauskas <donatas.petr@gmail.com>
2024-12-10 00:27:27 +0200

    day 9 part 2

--- 2024/day09.js
+++ 2024/day09.js
@@ -1,50 +1,106 @@
+const assert = require('assert')
 const fs = require('fs')
 
 let spec = fs.readFileSync(0, {encoding:'utf-8'}).trim()
+  .split('\n')[0]
   .split('').map(Number)
 
-let size = 0
+;(function part1() {
+  let size = 0
 
-for (let i = 0; i < spec.length; i++) {
-  size += spec[i]
-}
-
-let disk = new Array(size)
-
-for (let i = 0, j = 0, id = 0; i < spec.length; i += 2) {
-  for (let k = 0; k < spec[i]; k++) {
-    disk[j++] = id
+  for (let i = 0; i < spec.length; i++) {
+    size += spec[i]
   }
-  id++
-  for (let k = 0; k < spec[i+1]; k++) {
-    disk[j++] = '.'
+
+  let disk = new Array(size)
+
+  for (let i = 0, j = 0, id = 0; i < spec.length; i += 2) {
+    for (let k = 0; k < spec[i]; k++) {
+      disk[j++] = id
+    }
+    id++
+    if (spec[i+1])
+      for (let k = 0; k < spec[i+1]; k++) {
+        disk[j++] = '.'
+      }
   }
-}
-
-let i = 0;
-let j = disk.length - 1;
-while (i < j) {
-  while (disk[i] != '.')
-    i += 1
-  while (disk[j] == '.')
-    j -= 1
-
-  if (i >= j)
-    break
 
   // console.log(disk.join(''))
 
-  disk[i] = disk[j]
-  disk[j] = '.'
-}
+  let i = 0;
+  let j = disk.length - 1;
+  while (i < j) {
+    while (i < disk.length && disk[i] !== '.')
+      i += 1
+    while (j > 0 && disk[j] === '.')
+      j -= 1
 
-// console.log(disk.join(''))
+    if (i >= j)
+      break
 
-let checksum = 0
-for ( let i = 0; i < disk.length && disk[i] != '.'; i++ ) {
-  checksum += disk[i] * i
-}
+    disk[i] = disk[j]
+    disk[j] = '.'
+  }
 
-console.log(checksum)
+  // console.log(disk.join(''))
 
-// lines.forEach((line) => process.stdout.write(line + '\n'))
+  let checksum = 0
+  for ( let i = 0; i < disk.length && disk[i] != '.'; i++ ) {
+    checksum += disk[i] * i
+  }
+
+  console.log(checksum)
+})()
+
+;(function part2() {
+  let disk = []
+
+  function str(disk) {
+    return disk.map(block => {
+      if (block.file)
+        return block.id.toString(36).repeat(block.file)
+      return '.'.repeat(block.free)
+    }).join('')
+  }
+
+  for (let i = 0, id = 0; i < spec.length; i += 2) {
+    disk.push({file: spec[i], id: id++})
+    spec[i+1] && disk.push({free: spec[i+1]})
+  }
+
+  // console.log(str(disk))
+  for (let j = disk.length-1; j > 0; j--) {
+    for (let i = 0; i < j; i++) {
+      if (disk[i].free >= disk[j].file) {
+        const free = disk[i].free - disk[j].file
+        disk[i] = { ...disk[j] }
+        disk[j] = { free: disk[j].file }
+        if (free > 0) {
+          j+=1
+          disk.splice(i+1, 0, { free })
+        }
+        // console.log(str(disk))
+        break;
+      }
+    }
+  }
+  // console.log(str(disk))
+
+  let checksum = 0
+  for (let i = 0, j = 0; i < disk.length; i++) {
+    if (disk[i].file)
+      for (let k = 0; k < disk[i].file; k++, j++) {
+        checksum += disk[i].id * j
+      }
+    if (disk[i].free)
+      j += disk[i].free
+  }
+
+  console.log(checksum)
+})()
+
+// too low 85588967694
+// too low 85738318393
+
+// too high 6379497256282
+//          6335972980679
