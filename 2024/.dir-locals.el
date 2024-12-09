3f7f5aa1a708a482eb5d91e0004570eeaef4b15c
Donatas Petrauskas <donatas.petr@gmail.com>
2024-12-10 00:27:02 +0200

    recompile of example save

--- 2024/.dir-locals.el
+++ 2024/.dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((js-ts-mode . ((after-save-hook recompile))))
+((js-ts-mode . ((after-save-hook recompile)))
+ (text-mode . ((after-save-hook recompile))))
