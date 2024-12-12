7137c924990ef1a96b5f12aa1b804d98b9c0f3e5
Donatas Petrauskas <donatasp@wix.com>
2024-12-12 15:18:51 +0200

    optimize

--- 2024/day12.js
+++ 2024/day12.js
@@ -5,18 +5,17 @@ const garden = fs.readFileSync(0, {encoding: 'utf-8'}).trim().split('\n')
 
 // console.log(garden)
 
-let taken = new Set
 let regions = []
 
 let available = new Set
-let cell_to_str = (rc) => rc.join(',')
-let str_to_cell = (str) => str.split(',').map(Number)
+let to_str = (rc) => rc.join(',')
+let of_str = (str) => str.split(',').map(Number)
 
 const in_bounds = (r, c) => 0 <= r && r < garden.length && 0 <= c && c < garden[0].length
 
 for (let r = 0; r < garden.length; r++) {
   for (let c = 0; c < garden[r].length; c++) {
-    available.add(cell_to_str([r, c]))
+    available.add(to_str([r, c]))
   }
 }
 
@@ -25,7 +24,7 @@ for (let r = 0; r < garden.length; r++) {
 const moves = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
 for (let cell of available) {
-  let queue = [str_to_cell(cell)]
+  let queue = [of_str(cell)]
   let region = new Set([cell])
 
   while (queue.length) {
@@ -34,10 +33,9 @@ for (let cell of available) {
       const nr = r + move[0]
       const nc = c + move[1]
       const ncell = [nr, nc]
-      // console.log([r, c], cell_to_str(ncell))
-      if (available.has(cell_to_str(ncell)) && in_bounds(nr, nc) && garden[r][c] === garden[nr][nc]) {
-        available.delete(cell_to_str(ncell))
-        region.add(cell_to_str(ncell))
+      if (available.has(to_str(ncell)) && in_bounds(nr, nc) && garden[r][c] === garden[nr][nc]) {
+        available.delete(to_str(ncell))
+        region.add(to_str(ncell))
         queue.push(ncell)
       }
     }
@@ -53,31 +51,40 @@ const w = garden[0].length
 
 function perimeter(region) {
   let p = 0
-  for (let r = -1; r < h + 1; r++) {
-    for (let c = -1; c < w + 1; c++) {
-      if (region.has(cell_to_str([r, c])) && !region.has(cell_to_str([r, c+1])))
+  let [rs, re, cs, ce] = region_bounds(region)
+  for (let r = rs-1; r <= re+1; r++) {
+    for (let c = cs-1; c <= ce+1; c++) {
+      if (region.has(to_str([r, c])) && !region.has(to_str([r, c+1])))
         p += 1
-      if (region.has(cell_to_str([r, c])) && !region.has(cell_to_str([r, c-1])))
+      if (region.has(to_str([r, c])) && !region.has(to_str([r, c-1])))
         p += 1
-      if (region.has(cell_to_str([r, c])) && !region.has(cell_to_str([r-1, c])))
+      if (region.has(to_str([r, c])) && !region.has(to_str([r-1, c])))
         p +=1
-      if (region.has(cell_to_str([r, c])) && !region.has(cell_to_str([r+1, c])))
+      if (region.has(to_str([r, c])) && !region.has(to_str([r+1, c])))
         p +=1
     }
   }
   return p
 }
 
+function region_bounds(region) {
+  return Array.from(region)
+    .map(of_str)
+    .reduce(
+      ([rs, re, cs, ce], [r, c]) => [Math.min(r, rs), Math.max(r, re), Math.min(c, cs), Math.max(c, ce)],
+      [Infinity, -Infinity, Infinity, -Infinity]
+    )
+}
+
 function sides(region) {
   let corners = 0
-
-  for (let r = -1; r < h + 1; r++) {
-    for (let c = -1; c < w + 1; c++) {
-      let x = region.has(cell_to_str([r - 1, c - 1]))
-      let y = region.has(cell_to_str([r - 1, c]))
-      let z = region.has(cell_to_str([r, c - 1]))
-      let w = region.has(cell_to_str([r, c]))
-
+  let [rs, re, cs, ce] = region_bounds(region)
+  for (let r = rs-1; r <= re+1; r++) {
+    for (let c = cs-1; c <= ce+1; c++) {
+      let x = region.has(to_str([r - 1, c - 1]))
+      let y = region.has(to_str([r - 1, c]))
+      let z = region.has(to_str([r, c - 1]))
+      let w = region.has(to_str([r, c]))
       switch (x + y + z + w) {
       case 3:
       case 1:
@@ -90,7 +97,6 @@ function sides(region) {
       }
     }
   }
-
   return corners
 }
 
